d9a360802d690abb83c503b728668bc42ad0bcac
63070188 <68292431+63070188@users.noreply.github.com>
2020-11-26 18:07:18 +0700

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -3,6 +3,6 @@ return array(
     'host' => 'itf26.mysql.database.azure.com',
     'username' => 'aphatsara836@itf26',
     'password' => 'Po0926245419',
-    'database' => 'ITFLab,
+    'database' => 'ITFLab',
 );
 ?>
